5dc1e876a3c96c10e122956ad345185ef8ec33ab
Frank WangMa <frankwangma@gmail.com>
2018-09-15 10:19:53 +1200

    Changed the controller to be Main, and will rename fxml file

--- Main.java
+++ Main.java
@@ -1,19 +1,23 @@
 package application;
 	
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 import javafx.application.Application;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.StackPane;
 import javafx.scene.text.Text;
 
 
 
+
 public class Main extends Application {
+	static Path relativePath = Paths.get("");
+	static String _workDir = relativePath.toAbsolutePath().toString();
+	
 	@Override
 	public void start(Stage primaryStage) {
 		try {
@@ -33,5 +37,13 @@ public class Main extends Application {
 	
 	public static void main(String[] args) {
 		launch(args);
+	}
+	
+	@FXML
+	private Text text;
+	
+	@FXML 
+	private void micTestButton() {
+		text.setText("Mic test 1 2 3");
 	}
 }
