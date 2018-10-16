0ee6b572eb13ad9868852a30e49ab077dbda6e02
Frank Wang Ma <fwan175@en-337651.uoa.auckland.ac.nz>
2018-10-16 16:19:16 +1300

    Added some comments

--- HowToUse.java
+++ HowToUse.java
@@ -5,12 +5,21 @@ import javafx.scene.control.Button;
 import javafx.scene.control.TextArea;
 import javafx.stage.Stage;
 
+/**
+ * This class is used to display the how-to-use manual in the GUI
+ * @author fwan175
+ *
+ */
 public class HowToUse extends AbstractController{
 	
 	@FXML private TextArea _displayUserManual;
 	@FXML private Button _closeButton;
 	
+	/**
+	 * This handles the event when the close button is pressed
+	 */
 	@FXML public void closeButtonListener() {
+		//Close the window
 		Stage stage = (Stage) _closeButton.getScene().getWindow();
 		stage.close();
 	}
