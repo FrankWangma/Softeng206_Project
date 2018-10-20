8c6be462992dea5c4f8b985a07dfe03aa4ad6108
xoneon <xoneonxero@gmail.com>
2018-10-20 23:14:53 +1300

    removed unneeded packages

--- Main.java
+++ Main.java
@@ -7,8 +7,6 @@ import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.FilenameFilter;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Collections;
